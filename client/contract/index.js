820f282771229787aa6c46fc8830a37081634f30
veektrie <102025110+veektrie@users.noreply.github.com>
2022-12-30 17:57:58 +0100

    usestate props for contract and wallet address

--- client/contract/index.js
+++ client/contract/index.js
@@ -1,3 +1,5 @@
 import contract from './AVAXGods.json';
-export const ADDRESS = '0xe701694AedA63d294e262aFBB9F6Dc7448955140'
+export const ADDRESS = '0xe701694AedA63d294e262aFBB9F6Dc7448955140';
+
+export const { abi: ABI } = contract;
 
