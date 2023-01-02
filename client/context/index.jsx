c0acf3e760225feddc83776b6615983b6ee91243
veektrie <102025110+veektrie@users.noreply.github.com>
2023-01-02 06:18:15 +0100

    components for battle created

--- client/context/index.jsx
+++ client/context/index.jsx
@@ -97,6 +97,8 @@ export const GlobalContextProvider = ({ children }) => {
         setShowAlert,
         battleName,
         setBattleName,
+        battleGround,
+        setBattleGround,
       }}
     >
       {children}
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,9 +4,13 @@ import CustomInput from './CustomInput';
 import CustomButton from './CustomButton';
 import Alert from './Alert';
 import GameLoad from './GameLoad';
-
+import ActionButton from './ActionButton';
+import Card from './Card';
+import GameInfo from './GameInfo';
+import PlayerInfo from './PlayerInfo';
 export {
    PageHOC,
   CustomInput,
-  CustomButton,Alert,GameLoad
+  CustomButton, Alert, GameLoad, ActionButton, Card, GameInfo
+  ,PlayerInfo
 };
